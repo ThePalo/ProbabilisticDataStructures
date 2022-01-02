f080e4551520fa1fe4507eb842526a5c42a7b986
ThePalo <gerard.palomares.c@est.fib.upc.edu>
2022-01-02 17:44:26 +0100

    bloomFilter is ready

--- bloomFilter/bloomFilter.go
+++ bloomFilter/bloomFilter.go
@@ -2,12 +2,13 @@ package bloomFilter
 
 import (
 	"math"
+	"math/bits"
 
 	"github.com/bits-and-blooms/bitset"
 	"github.com/spaolacci/murmur3"
 )
 
-
+const machine64Bits = 64
 
 type BloomFilter struct {
 	n uint
@@ -17,34 +18,38 @@ type BloomFilter struct {
 	bits *bitset.BitSet
 }
 
-func New(size uint, error float64) BloomFilter {
-	sizeM := computeSizeM(size, error)
-	sizeK := computeSizeK(size, sizeM)
-
+func newBF(n uint, e float64, m uint, k uint) BloomFilter {
 	return BloomFilter{
-		n: size,
-		e: error,
-		m: sizeM,
-		k: sizeK,
-		bits: bitset.New(sizeM),
+		n: n,
+		e: e,
+		m: m,
+		k: k,
+		bits: bitset.New(m),
 	}
 }
 
+// New creates a new Bloom Filter
+func New(m uint, k uint) BloomFilter {
+	n := computeCapacity(m, k)
+	e := computeError(m, k, n)
+	return newBF(n, e, m, k)
+}
+
+func NewFromSizeAndError(size uint, error float64) BloomFilter {
+	sizeM := computeSizeM(size, error)
+	sizeK := computeSizeK(size, sizeM)
+	return newBF(size, error, sizeM, sizeK)
+}
+
 func (b *BloomFilter) Insert(element []byte) {
-	positions, err := b.computeHashPositions(element)
-	if err != nil {
-		panic(err)
-	}
+	positions := b.computeKHashPositions(element)
 	for _, pos := range positions {
 		b.bits.Set(pos)
 	}
 }
 
 func (b BloomFilter) Lookup(element []byte) bool {
-	positions, err := b.computeHashPositions(element)
-	if err != nil {
-		panic(err)
-	}
+	positions := b.computeKHashPositions(element)
 	for _, pos := range positions {
 		if !b.bits.Test(pos) {
 			return false
@@ -53,19 +58,34 @@ func (b BloomFilter) Lookup(element []byte) bool {
 	return true
 }
 
+func (b BloomFilter) computeKHashPositions(element []byte) []uint {
+	positions := make([]uint, 0)
+	for i := uint(0); i < b.k; i++ {
+		pos := computeHash(element, uint32(i)) % b.m
+		positions = append(positions, pos)
+	}
+	return positions
+}
+
+func computeHash(element []byte, seed uint32) uint {
+	if bits.UintSize == machine64Bits {
+		return uint(murmur3.Sum64WithSeed(element, seed))
+	}
+	return uint(murmur3.Sum32WithSeed(element, seed))
+}
+
+func computeCapacity(m uint, k uint) uint {
+	return uint(math.Floor(float64(m)/(float64(k))*math.Log(2)))
+}
+
+func computeError(m uint, k uint, n uint) float64 {
+	return math.Pow(1-math.Pow(math.E, -float64(k)*float64(n)/float64(m)), float64(k))
+}
+
 func computeSizeM(n uint, error float64) uint {
 	return uint(math.Ceil(-(float64(n)*math.Log(error))/(math.Log(2)*math.Log(2))))
 }
 
 func computeSizeK(n uint, sizeM uint) uint {
 	return uint(math.Ceil((float64(sizeM)/float64(n))*math.Log(2)))
-}
-
-func (b BloomFilter) computeHashPositions(element []byte) ([]uint, error) {
-	positions := make([]uint, 0)
-	for i := uint(0); i < b.k; i++ {
-		pos := uint(murmur3.Sum64WithSeed(element, uint32(i)))%b.m
-		positions = append(positions, pos)
-	}
-	return positions, nil
 }
--- bloomFilter/bloomFilter_test.go
+++ bloomFilter/bloomFilter_test.go
@@ -1,9 +1,70 @@
 package bloomFilter
 
-import "testing"
+import (
+	"fmt"
+	"math"
+	"testing"
+)
 
-func TestBasic(t *testing.T) {
-	b := New(100, 0.03)
+const (
+	roundTo = 100000
+	errorRangeFalsePositives = 0.1
+)
+
+func TestNew(t *testing.T) {
+	bFList := []BloomFilter{
+		{
+			n: 84,
+			m: 730,
+			k: 6,
+			e: 0.015370,
+		},
+	}
+	for _, expectedBF := range bFList {
+		bf := New(expectedBF.m, expectedBF.k)
+		if bf.n != expectedBF.n {
+			t.Errorf("Expected capacity %d, Current capacity %d", expectedBF.n, bf.n)
+		}
+		if math.Round(bf.e*roundTo)/roundTo != math.Round(expectedBF.e*roundTo)/roundTo {
+			t.Errorf("Expected error %.5f, Current error %.5f", expectedBF.e, bf.e)
+		}
+		if bf.m != expectedBF.m {
+			t.Errorf("Expected size %d, Current size %d", expectedBF.m, bf.m)
+		}
+		if bf.k != expectedBF.k {
+			t.Errorf("Expected # hash functions %d, Current # hash functions %d", expectedBF.k, bf.k)
+		}
+	}
+}
+
+func TestNewFromSizeAndError(t *testing.T) {
+	bFList := []BloomFilter{
+		{
+			n: 100,
+			m: 730,
+			k: 6,
+			e: 0.03,
+		},
+	}
+	for _, expectedBF := range bFList {
+		bf := NewFromSizeAndError(expectedBF.n, expectedBF.e)
+		if bf.n != expectedBF.n {
+			t.Errorf("Expected capacity %d, Current capacity %d", expectedBF.n, bf.n)
+		}
+		if math.Round(bf.e*roundTo)/roundTo != math.Round(expectedBF.e*roundTo)/roundTo {
+			t.Errorf("Expected error %.5f, Current error %.5f", expectedBF.e, bf.e)
+		}
+		if bf.m != expectedBF.m {
+			t.Errorf("Expected size %d, Current size %d", expectedBF.m, bf.m)
+		}
+		if bf.k != expectedBF.k {
+			t.Errorf("Expected # hash functions %d, Current # hash functions %d", expectedBF.k, bf.k)
+		}
+	}
+}
+
+func TestInsertAndLookup(t *testing.T) {
+	b := NewFromSizeAndError(100, 0.03)
 	elem1 := []byte("Hello World")
 	elem2 := []byte("A")
 	elem3 := []byte("Bye")
@@ -17,5 +78,35 @@ func TestBasic(t *testing.T) {
 	}
 	if ok := b.Lookup(elem3); ok {
 		t.Errorf("%s should NOT be in.", elem3)
+	}
+}
+
+func TestInsertAndLookupWithHighLoadFactor(t *testing.T) {
+	size := uint(100)
+	e := 0.03
+	b := NewFromSizeAndError(size, e)
+	listElement := make([]string, size)
+	for i := uint(0); i < size; i++ {
+		listElement[i] = fmt.Sprintf("%d", i)
+		b.Insert([]byte(listElement[i]))
+	}
+	// Look up for elements already in the filter
+	for _, elem := range listElement {
+		if ok := b.Lookup([]byte(elem)); !ok {
+			t.Errorf("%s should be in.", elem)
+		}
+	}
+	// Look up for elements that NOT exists in the filter
+	falsePositives := uint(0)
+	for i := size; i < 2*size; i++ {
+		elem := fmt.Sprintf("%d", i)
+		if ok := b.Lookup([]byte(elem)); ok {
+			falsePositives++
+		}
+	}
+	expectedFalsePositives := uint(float64(size) * e)
+	rangeFalsePositives := uint(math.Ceil(float64(expectedFalsePositives) * errorRangeFalsePositives))
+	if uint(math.Abs(float64(falsePositives - expectedFalsePositives))) < rangeFalsePositives {
+		t.Errorf("Error: Expected false positives are %d ± %d and current false positives are %d", expectedFalsePositives, rangeFalsePositives, falsePositives)
 	}
 }
